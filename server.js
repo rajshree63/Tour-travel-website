8a7abb7ca995c43b0de2f3efe040740b4fbed02c
Raj Shree <mamtadevi963161@gmail.com>
2026-05-26 14:42:36 +0530

    Fixed Render deployment

--- server.js
+++ server.js
@@ -1,87 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Booking = require("./models/Booking");
 
 const app = express();
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 /* Middleware */
-
 app.use(express.static(__dirname));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-
 /* MongoDB Connection */
-
-mongoose.connect(
-    "mongodb+srv://mamtadevi963161_db_user:LEW8Sux0aWDA99DV@cluster0.xah1qv5.mongodb.net/tourDB?retryWrites=true&w=majority"
-)
-
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-
     console.log("MongoDB Connected");
-
 })
-
 .catch((err) => {
-
-    console.log("MongoDB Error:", err);
-
+    console.log(err);
 });
-
 
 /* Home Route */
-
 app.get("/", (req, res) => {
-
     res.sendFile(__dirname + "/index.html");
-
 });
 
-
-/* Server Start */
-/* Booking Route */
-
-app.post("/book", async (req, res) => {
-
-    try {
-
-        const newBooking = new Booking({
-
-            name: req.body.name,
-
-            email: req.body.email,
-
-            package: req.body.package,
-
-            travelers: req.body.travelers,
-
-            date: req.body.date
-
-        });
-
-        await newBooking.save();
-
-        res.send("Booking Successful!");
-
-    }
-
-    catch (error) {
-
-        console.log(error);
-
-        res.send("Error saving booking");
-
-    }
-
-});
+/* Start Server */
 app.listen(PORT, () => {
-
     console.log(`Server running on port ${PORT}`);
-
 });

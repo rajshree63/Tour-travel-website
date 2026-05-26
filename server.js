c885d428cb89c305ea86f12e88eef483dfae2723
Raj Shree <mamtadevi963161@gmail.com>
2026-05-26 15:07:20 +0530

    Fixed Render PORT

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
 });
 
 /* Start Server */
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
